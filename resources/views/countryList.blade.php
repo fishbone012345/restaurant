fb71fb758b90fd8644cf8c900e5c3ab96f27a96c
Chieh-Hsiang Hu <chieh-hsianghu@Chieh-Hsiangde-MacBook-Pro.local>
2020-12-18 14:28:21 +0800

    test

---
+++ resources/views/countryList.blade.php
@@ -0,0 +1,30 @@
+@extends('layout')
+@section('content')
+    <div class="regtangle7">
+    	<a href="">
+    		<p id="taiwanText">Taiwan</p>
+    		<img src="/image/Taiwan.jpg" id="taiwanImage">
+    	</a>
+        <a href="">
+        	<p id="indonesiaText">Indonesia</p>
+        	<img src="/image/indonisa.jpg" id="indonesiaImage">
+        </a>
+        <a href="">
+        	<p id="japanText">Japan</p>
+        	<img src="/image/T.jpg" id="japanImage">
+        </a>
+        <a href="">
+        	<p id="koreaText">Korea</p>
+        	<img src="/image/T.jpg" id="koreaImage">
+        </a>
+        <a href="">
+        	<p id="singaporeText">Singapore</p>
+        	<img src="/image/T.jpg" id="singaporeImage">
+        </a>	
+        <a href="">
+        	<p id="chinaText">China</p>
+        	<img src="/image/T.jpg" id="chinaImage">
+        </a>	
+        	
+    </div>
+@stop
---
+++ resources/views/layout.blade.php
@@ -0,0 +1,20 @@
+<html>
+	<head>
+		<link rel=stylesheet type="text/css" href="restaurant.css">
+	</head>
+    <body>
+    	<div id="bar">
+    		<img src="/image/restaurant-cutlery-circular-symbol-of-a-spoon-and-a-fork-in-a-circle.png" id="icon">
+    		<p id="restaurantInAsia">Restaurant in Asia</p> 
+    		<button id="option">Option</button>
+    		<a href="" id="register">Register註冊</a>
+	        <p id="line">|</p>
+	        <a href="" id="login">Login登入</a>
+	        <img src="/image/user (1).png" id="userImage">
+        </div>
+        <div id="regtangle4">
+        </div>
+
+        @yield('content')
+    </body>
+</html>
